57838df83109c8dbdd282f85d8a4542faaf95939
Alejandro Ortega <cristian-forero@upc.edu.co>
2023-11-06 22:09:10 -0500

    Fix Route Map Item aditional fix the roundedRectangles.

---
+++ PPoint/View/MapItem/mapItem.swift
@@ -0,0 +1,18 @@
+//
+//  mapItem.swift
+//  PPoint
+//
+//  Created by A. Ortega on 11/6/23.
+//
+
+import SwiftUI
+
+struct mapItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mapItem()
+}
